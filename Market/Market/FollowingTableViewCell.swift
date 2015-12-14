6d13e304f23522e8d13dcbeb704d6ea4d28a4291
Minh Dinh <dtminhpt@gmail.com>
2015-12-14 11:30:06 +0700

    Following settings

---
+++ Market/Market/FollowingTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FollowingTableViewCell.swift
+//  Market
+//
+//  Created by Dinh Thi Minh on 12/14/15.
+//  Copyright © 2015 MDT Group. All rights reserved.
+//
+
+import UIKit
+
+class FollowingTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imgField: UIImageView!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var unfollowingButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
