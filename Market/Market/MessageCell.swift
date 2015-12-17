23f1059f3a0837de9f535a2cfb69b1d8916b5f90
Minh Dinh <dtminhpt@gmail.com>
2015-12-17 22:11:13 +0700

    Adding Message screen and Chat screen

---
+++ Market/Market/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Market
+//
+//  Created by Dinh Thi Minh on 12/17/15.
+//  Copyright © 2015 MDT Group. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userFullname: UILabel!
+    @IBOutlet weak var TimeElapedLabel: UILabel!
+    @IBOutlet weak var lastMessageLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Market/Market/MessageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MessageViewController.swift
+//  Market
+//
+//  Created by Dinh Thi Minh on 12/17/15.
+//  Copyright © 2015 MDT Group. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
