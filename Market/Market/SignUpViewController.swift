5b4f286ec3a0e902737c69e89ccfaeb4990b328e
Ngo Thanh Tai <ngothanhtai89@yahoo.com>
2015-12-20 15:39:42 +0700

    Just commit to merge with branch minh

--- Market/Market/SignUpViewController.swift
+++ Market/Market/SignUpViewController.swift
@@ -21,8 +21,61 @@ class SignUpViewController: UIViewController {
         spinner = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150))
         
         //set focus to fullnameField
-        self.fullnameField.becomeFirstResponder()
+       // self.fullnameField.becomeFirstResponder()
+        
+        // Add observer to detect when the keyboard will be shown
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        
+        
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        self.view.addGestureRecognizer(tap)
+        
+
     }
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        self.view.endEditing(true)
+        print("The keyboard is dismissed")
+        
+    }
+    
+    
+    
+    /*MARK: Fix bug when keyboard slides up*/
+    
+    func keyboardWillShow(sender: NSNotification) {
+        let userInfo: [NSObject : AnyObject] = sender.userInfo!
+        
+        let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        let offset: CGSize = userInfo[UIKeyboardFrameEndUserInfoKey]!.CGRectValue.size
+        
+        if keyboardSize.height == offset.height {
+            if self.view.frame.origin.y == 0 {
+                UIView.animateWithDuration(0.1, animations: { () -> Void in
+                    self.view.frame.origin.y -= keyboardSize.height/2
+                })
+            }
+        } else {
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                self.view.frame.origin.y += keyboardSize.height/2 - offset.height
+            })
+        }
+        print("Keyboard will show and new position y of View",self.view.frame.origin.y)
+        
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        let userInfo: [NSObject : AnyObject] = sender.userInfo!
+        let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        self.view.frame.origin.y += keyboardSize.height/2
+        print("Keyboard will hide")
+        
+    }
+
     
     @IBAction func signUpTap(sender: AnyObject) {
         
--- Market/Market/SimplifiedItemCell.swift
+++ Market/Market/SimplifiedItemCell.swift
@@ -9,49 +9,74 @@
 import UIKit
 
 class SimplifiedItemCell: UITableViewCell {
-  
-  @IBOutlet weak var itemImageView: UIImageView!
-  @IBOutlet weak var itemNameLabel: UILabel!
-  @IBOutlet weak var priceLabel: UILabel!
-  @IBOutlet weak var postAtLabel: UILabel!
-  
-  var item: Post! {
-    didSet {
-      let post = item
-      
-      // Set Item
-      if post.medias.count > 0 {
-        itemImageView.alpha = 0.0
-        UIView.animateWithDuration(0.3, animations: {
-          self.itemImageView.setImageWithURL(NSURL(string: post.medias[0].url!)!)
-          self.itemImageView.alpha = 1.0
-          }, completion: nil)
-      } else {
-        // Load no image
-      }
-      itemNameLabel.text = post.title
-      
-      let formatter = NSDateFormatter()
-      formatter.timeStyle = NSDateFormatterStyle.ShortStyle
-      formatter.dateStyle = NSDateFormatterStyle.MediumStyle
-      postAtLabel.text = "@ \(formatter.stringFromDate(post.createdAt!))"
-      
-      priceLabel.text = post.price.formatCurrency()
-      //      newTagImageView.hidden = (post.condition > 0)
-    }
-  }
-  
-  override func awakeFromNib() {
-    super.awakeFromNib()
-    // Initialization code
-    itemImageView.layer.cornerRadius = 5
-    itemImageView.clipsToBounds = true
-  }
-  
-  override func setSelected(selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
     
-    // Configure the view for the selected state
-  }
-  
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var postAtLabel: UILabel!
+    @IBOutlet weak var newTagImageView: UIImageView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var sellerLabel: UILabel!
+    
+    var item: Post! {
+        didSet {
+            let post = item
+            
+            // Set seller
+            //      if post.user.objectId != User.currentUser()?.objectId {
+            sellerLabel.text = post.user.fullName
+            if let avatar = post.user.avatar {
+                avatarImageView.alpha = 0.0
+                avatarImageView.image = nil
+                UIView.animateWithDuration(0.3, animations: {
+                    self.avatarImageView.setImageWithURL(NSURL(string: avatar.url!)!)
+                    self.avatarImageView.alpha = 1.0
+                })
+            } else {
+                // load no image
+            }
+            //      } else {
+            //        avatarImageView.hidden = true
+            //        sellerLabel.hidden = true
+            //      }
+            
+            // Set Item
+            if post.medias.count > 0 {
+                itemImageView.alpha = 0.0
+                UIView.animateWithDuration(0.3, animations: {
+                    self.itemImageView.setImageWithURL(NSURL(string: post.medias[0].url!)!)
+                    self.itemImageView.alpha = 1.0
+                    }, completion: nil)
+            } else {
+                // Load no image
+            }
+            itemNameLabel.text = post.title
+            
+            //      let formatter = NSDateFormatter()
+            //      formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            //      formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            //      postAtLabel.text = "@ \(formatter.stringFromDate(post.updatedAt!))"
+            postAtLabel.text = Helper.timeSinceDateToNow(post.updatedAt!)
+            priceLabel.text = post.price.formatCurrency()
+            newTagImageView.hidden = (post.condition > 0)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        itemImageView.layer.cornerRadius = 5
+        itemImageView.clipsToBounds = true
+        priceLabel.layer.cornerRadius = 5
+        priceLabel.clipsToBounds = true
+        avatarImageView.layer.cornerRadius = 16
+        avatarImageView.clipsToBounds = true
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
 }
